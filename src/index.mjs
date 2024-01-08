9167f5d1698cda4513ede63dbd4c1d14373108b2
Jumpei Funato <funato.jumpei@tis.co.jp>
2024-01-08 15:07:23 +0900

    追加ボタンを押してもうまく作用しない

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,9 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+    alert();
+}
+//getElementByIDドキュメントにもともとあるメソッド。IDによってエレメントを取得する。
+//addEventListener ボタンが押された後のイベントを検知するメソッド。一つ目の引数は検知するイベント、二つ目の引数は実行するイベントをとる
+document.getElementById("add-button").addEventListener("click", onClickAdd);
+
